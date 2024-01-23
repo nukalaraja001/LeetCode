95c15b3b55100e6845aa4378b54cc6f278a27c3a
Nukala, Raja | RIEPL <raja.nakula@rakuten.com>
2024-01-23 17:02:09 +0530

    Initial commit

---
+++ LeetCode/LeetCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeetCodeApp.swift
+//  LeetCode
+//
+//  Created by Nukala, Raja | RIEPL on 23/01/24.
+//
+
+import SwiftUI
+
+@main
+struct LeetCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
